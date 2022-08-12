d5d09ee660a60767d4ea4fba46e79bea4082481c
vladi <vladi.konov@gmail.com>
2022-08-12 18:52:40 +0300

    fix issue

--- src/test/java/org/example/UserRepositoryTest.java
+++ src/test/java/org/example/UserRepositoryTest.java
@@ -1,10 +1,22 @@
-package org.example.Repository;
-
-import org.example.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@DataJpaTest
- public interface UserRepositoryTest extends JpaRepository<User, Long> {
-
-}
+//package org.example;
+//
+//import org.example.User;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.test.annotation.Rollback;
+//
+////@SpringBootTest
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Rollback
+//class UserManagerTests {
+//
+// @Test
+// void contextLoads() {
+// }
+//
+//}
